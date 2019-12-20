fc6019b0b1a35b33dcde6e9c18d4dfb728e49675
weidian <wudandan>
2019-12-20 14:19:39 +0800

    “xxx”

---
+++ src/main/java/com/wdd/test.java
@@ -0,0 +1,9 @@
+package com.wdd;
+
+public class test {
+    static String  name = "wudandan";
+    static int  age = 3;
+    public static void main(String[] args) {
+        System.out.println(name);
+    }
+}
