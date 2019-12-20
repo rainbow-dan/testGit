c2b5af77ef078f96abfe1ab605bdedbdcfa8637c
weidian <wudandan>
2019-12-20 15:04:38 +0800

    第二次更改

--- src/main/java/com/wdd/test.java
+++ src/main/java/com/wdd/test.java
@@ -3,6 +3,8 @@ package com.wdd;
 public class test {
     static String  name = "wudandan";
     static int  age = 3;
+    static int length = 4;
+    String  sex = "man";
     public static void main(String[] args) {
         System.out.println(name);
     }
